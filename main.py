edc337fa56de0d3441f0aa6d1dcd66a5ab246e09
Paweł Jędrszczyk <pjedrszczyk@student.agh.edu.pl>
2021-10-16 19:49:53 +0200

    postępy

--- main.py
+++ main.py
@@ -3,17 +3,7 @@ import scipy
 import math
 
 def cylinder_area(r:float,h:float):
-    """Obliczenie pola powierzchni walca. 
-    Szczegółowy opis w zadaniu 1.
-    
-    Parameters:
-    r (float): promień podstawy walca 
-    h (float): wysokosć walca
-    
-    Returns:
-    float: pole powierzchni walca 
-    """
-    if  isinstance(r, float) or isinstance(h, float) or r >= 0 or h >= 0:
+    if  isinstance(r, float) and isinstance(h, float) and r >= 0 and h >= 0:
         pp = np.pi * r**2
         pb = 2 * np.pi * r * h
         pc = 2*pp + pb
@@ -21,6 +11,7 @@ def cylinder_area(r:float,h:float):
         return pc
     else:
         return None
+
 
 def fib(n:int):
     """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego. 
@@ -32,31 +23,37 @@ def fib(n:int):
     Returns:
     np.ndarray: wektor n pierwszych wyrazów ciągu Fibonnaciego.
     """
-    return None
+    if n == 1 or n ==2:
+        return 1
+    elif n>2:
+        return fib(n-1)+fib(n-2)
+    else:
+        return None
+#print(fib(6))
 
 def matrix_calculations(a:float):
-    """Funkcja zwraca wartości obliczeń na macierzy stworzonej 
-    na podstawie parametru a.  
-    Szczegółowy opis w zadaniu 4.
+    M = np.array([[a, 1, -a], [0, 1, 1], [-a, a, 1]])
+    Minv = np.linalg.inv(M)
+    Mt = M.T
+    Mdet = np.linalg.det(M)
+    return (Minv, Mt, Mdet)
+
+
     
-    Parameters:
-    a (float): wartość liczbowa 
-    
-    Returns:
-    touple: krotka zawierająca wyniki obliczeń 
-    (Minv, Mt, Mdet) - opis parametrów w zadaniu 4.
-    """
-    return None
 
 def custom_matrix(m:int, n:int):
-    """Funkcja zwraca macierz o wymiarze mxn zgodnie 
-    z opisem zadania 7.  
+    wynik = np.zeros((m,n))
     
-    Parameters:
-    m (int): ilość wierszy macierzy
-    n (int): ilość kolumn macierzy  
-    
-    Returns:
-    np.ndarray: macierz zgodna z opisem z zadania 7.
-    """
-    return None
+    if m>0 and n>0:
+        for i in range(m):
+            for j in range(n):
+                if i>j:
+                    wynik[i, j] = i
+                else:
+                    wynik[i, j] = j
+        return wynik
+    else:
+        return None
+#print("lalaa")       
+#print(custom_matrix(4,6))
+
